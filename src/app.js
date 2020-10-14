738967e14f7c429a81a7999c23769624e298709d
Carlos Augusto <carlosaugustoss2128@gmail.com>
2020-10-14 14:48:53 -0300

    Starting server project Stock.

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+import Routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+    this.server.use(cors());
+  }
+
+  routes() {
+    this.server.use(Routes);
+  }
+}
+
+export default new App().server;
